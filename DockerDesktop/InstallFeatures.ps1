9d9d66fb15cb229673c80a175e5080a755fab1ee
Grzegorz Ochlik <grzegorz.ochlik@gmail.com>
2020-05-11 12:10:52 +0200

    DockerDesktop on Win10

---
+++ DockerDesktop/InstallFeatures.ps1
@@ -0,0 +1,16 @@
+#Requires -RunAsAdministrator
+
+#Based on @talon script
+function Import-Feature {
+    param( $Name )
+    
+    $packages = Get-ChildItem "C:\Windows\servicing\Packages\*$Name*.mum"
+    foreach ($package in $packages) {
+        Dism /online /NoRestart /Add-Package:"$package"
+    }
+    Dism /online /NoRestart /Enable-Feature /FeatureName:$Name -All /LimitAccess /ALL
+}
+
+Import-Feature -Name Microsoft-Hyper-V
+Import-Feature -Name Containers 
+Write-Host "Restart computer now!";
---
+++ DockerDesktop/TempWinPro.ps1
@@ -0,0 +1,8 @@
+$currVer = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion';
+$edId = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').EditionID;
+
+Set-ItemProperty -Path $currVer -Name 'EditionID' -Value 'Professional';
+Write-Host "You temporarily have Win10 Pro, congratulations (prev edition: $edId)!";
+Write-Host 'You may now run the Docker Desktop Installer. After installation is finished - you can confirm the dialog window to revert changes';
+Set-ItemProperty -Path $currVer -Name 'EditionID' -value $edId -Confirm;
+Write-Host "`nScript finished!";
