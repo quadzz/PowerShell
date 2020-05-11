6ecfe841aa3d00c92aafaf5cc65970fdc94615e1
Grzegorz Ochlik <grzegorz.ochlik@gmail.com>
2020-05-11 14:48:34 +0200

    Make script safer

--- DockerDesktop/2. TempWinPro.ps1
+++ DockerDesktop/2. TempWinPro.ps1
@@ -3,8 +3,14 @@
 $currVer = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion';
 $edId = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion').EditionID;
 
-Set-ItemProperty -Path $currVer -Name 'EditionID' -Value 'Professional';
-Write-Host "You temporarily have Win10 Pro, congratulations (prev edition: $edId)!";
-Write-Host 'You may now run the Docker Desktop Installer. After installation is finished - you can confirm the dialog window to revert changes';
-Set-ItemProperty -Path $currVer -Name 'EditionID' -value $edId -Confirm;
+if ($edId -ne "Professional") {
+    Set-ItemProperty -Path $currVer -Name 'EditionID' -Value 'Professional';
+    Write-Host "You temporarily have Win10 Pro, congratulations (prev edition: $edId)!";
+    Write-Host 'You may now run the Docker Desktop Installer. After installation is finished - you can confirm the dialog window to revert changes';
+    Set-ItemProperty -Path $currVer -Name 'EditionID' -value $edId -Confirm;
+}
+else {
+    Write-Warning "Edition is already Professional"
+}
+
 Write-Host "`nScript finished!";
